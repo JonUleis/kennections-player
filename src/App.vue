3b2a089f6135f21f79ce36ebf76c6ebeba48da63
Jon Uleis <hi@jonuleis.com>
2022-06-21 16:56:56 -0400

    Check answers with fuzzy logic, introduction, footer, blanks toggle, Kennection image

--- src/App.vue
+++ src/App.vue
@@ -4,60 +4,116 @@
     <span v-else>Connecting...</span>
   </div>
   <div class="game" v-else>
-    <h1>{{ puzzleJson.title }}</h1>
-    <h2>
-      by
-      <a :href="puzzleJson.authors[0].link" target="_blank">
-        {{ puzzleJson.authors[0].name }}
-      </a>
-    </h2>
+    <header class="header">
+      <h1>Kennections Player</h1>
+      <p>
+        Play the new slideshow-style
+        <a href="https://www.mentalfloss.com/kennections" target="_blank"
+          >Kennections</a
+        >
+        games introduced in March 2022 interactively!
+      </p>
+      <p>
+        Note that the new Kennections no longer include alternate accepted
+        answers like the previous interactive version (eg. "steaks", "types of
+        steak"), but some fuzzy matching will be performed while answering.
+      </p>
+      <p>
+        <label>
+          <input type="checkbox" v-model="showBlanks" /> Show — — — — answer
+          blanks before typing
+        </label>
+      </p>
+    </header>
+    <div class="content">
+      <h1>{{ puzzleJson.title }}</h1>
+      <h2>
+        by
+        <a :href="puzzleJson.authors[0].link" target="_blank">
+          {{ puzzleJson.authors[0].name }}
+        </a>
+        &bull;
+        {{ puzzleJson.createdAt }}
+      </h2>
 
-    <label>
-      <span v-html="questions[0]" />
-      <input type="text" v-model="input1" />
-    </label>
-    <label>
-      <span v-html="questions[1]" />
-      <input type="text" v-model="input2" />
-    </label>
-    <label>
-      <span v-html="questions[2]" />
-      <input type="text" v-model="input3" />
-    </label>
-    <label>
-      <span v-html="questions[3]" />
-      <input type="text" v-model="input4" />
-    </label>
-    <label>
-      <span v-html="questions[4]" />
-      <input type="text" v-model="input5" />
-    </label>
+      <label v-for="(e, i) in inputs" :key="`q-${i}`">
+        <span v-if="i < 5" v-html="questions[i]" />
+        <b v-else>What's the Kennection?</b>
 
-    <label>
-      <b>What's the Kennection?</b>
-      <input type="text" v-model="inputKennection" />
-    </label>
+        <div class="input">
+          <div class="answer" v-if="submitted || correct[i]">
+            <span class="incorrect" v-if="!correct[i]">
+              {{
+                inputs[i]
+                  ? `You answered: ${inputs[i]}.`
+                  : "You did not answer."
+              }}
+            </span>
+            <span class="correct">
+              {{ correct[i] ? "Correct:" : "The correct answer is:" }}
+              {{ answers[i] }}
+            </span>
+          </div>
+          <input
+            v-else
+            v-model="inputs[i]"
+            @keyup="changed(i)"
+            @change="changed(i)"
+            :placeholder="showBlanks ? blanks[i] : ''"
+            type="text"
+          />
+        </div>
+      </label>
+
+      <div class="buttons" v-if="!submitted">
+        <button @click="solve">I GIVE UP</button>
+        <button @click="solve">SUBMIT</button>
+      </div>
+
+      <img
+        v-else-if="image"
+        class="image"
+        :src="`${image.host}${image.path}`"
+        :alt="image.alt"
+      />
+      <span v-if="submitted && image?.credit" v-html="image.credit" />
+    </div>
+    <nav class="nav">
+      <span>2022 Kennections Games</span>
+      <ul v-if="latest % 10">
+        <li v-for="i in latest % 10" :key="`nav-${i}`">
+          <a
+            @click="puzzle = latest - i + 1"
+            :class="{ active: puzzle == latest - i + 1 }"
+            >{{ latest - i + 1 }}</a
+          >
+        </li>
+      </ul>
+      <ul v-for="list in navRows" :key="`nav-${list}`">
+        <li v-for="i in 10" :key="`nav-${i}`">
+          <a
+            @click="puzzle = (list + 1) * 10 - i + 1"
+            :class="{ active: puzzle == (list + 1) * 10 - i + 1 }"
+            >{{ list == 0 && i > 1 ? "0" : "" }}{{ (list + 1) * 10 - i + 1 }}</a
+          >
+        </li>
+      </ul>
+    </nav>
+    <footer class="footer">
+      Kennections Player by
+      <a href="https://twitter.com/MovingToTheSun" target="_blank">Jon Uleis</a
+      >. Source code on
+      <a href="https://github.com/JonUleis/kennections-player" target="_blank"
+        >GitHub</a
+      >.
+      <br />
+      Games are loaded from
+      <a href="https://www.mentalfloss.com/kennections" target="_blank"
+        >Mental Floss</a
+      >
+      and not stored on this server.
+    </footer>
   </div>
-  <nav class="nav">
-    <ul v-if="latest % 10">
-      <li v-for="i in latest % 10" :key="`nav-${i}`">
-        <a
-          @click="puzzle = latest - i + 1"
-          :class="{ active: puzzle == latest - i + 1 }"
-          >{{ latest - i + 1 }}</a
-        >
-      </li>
-    </ul>
-    <ul v-for="list in navRows" :key="`nav-${list}`">
-      <li v-for="i in 10" :key="`nav-${i}`">
-        <a
-          @click="puzzle = (list + 1) * 10 - i + 1"
-          :class="{ active: puzzle == (list + 1) * 10 - i + 1 }"
-          >{{ list == 0 && i > 1 ? "0" : "" }}{{ (list + 1) * 10 - i + 1 }}</a
-        >
-      </li>
-    </ul>
-  </nav>
 </template>
 
 <script>
@@ -69,17 +125,16 @@ export default {
     return {
       puzzle: 0,
       latest: 0,
-      puzzleJson: {},
-      input1: "",
-      input2: "",
-      input3: "",
-      input4: "",
-      input5: "",
-      inputKennection: "",
-      questions: Array(5),
-      answers: Array(5),
-      kennection: "",
       navRows: 0,
+      puzzleJson: {},
+      inputs: Array(6),
+      correct: Array(6),
+      questions: Array(5),
+      blanks: Array(5),
+      answers: Array(6),
+      showBlanks: false,
+      submitted: false,
+      image: null,
     };
   },
   mounted() {
@@ -100,8 +155,37 @@ export default {
     },
   },
   methods: {
+    changed(el) {
+      if (el == 5) {
+        // don't auto-submit kennection
+        if (!this.submitted) {
+          return false;
+          // if submitted, match kennection if answer is 4+ letters long and contained in real kennection
+        } else if (
+          this.fuzzy(this.inputs[el]).length >= 4 &&
+          this.answers[el].toLowerCase().includes(this.fuzzy(this.inputs[el]))
+        ) {
+          this.correct[el] = true;
+        }
+      } else if (
+        // check if input contains the whole answer
+        this.fuzzy(this.inputs[el]).includes(this.fuzzy(this.answers[el]))
+      ) {
+        this.correct[el] = true;
+      }
+    },
+    solve() {
+      this.submitted = true;
+      for (let i = 0; i < this.inputs.length; i++) {
+        this.changed(i);
+      }
+    },
     getPuzzle() {
       this.puzzleJson = {};
+      this.inputs = Array(5);
+      this.correct = Array(5);
+      this.submitted = false;
+      this.image = null;
       axios
         .post(`${process.env.VUE_APP_PHP}php/`, { puzzle: this.puzzle })
         .then((res) => {
@@ -129,25 +213,38 @@ export default {
               this.clean(this.puzzleJson.body[qIndex + 6].html),
               this.clean(this.puzzleJson.body[qIndex + 8].html),
             ];
+            this.blanks = [
+              this.clean(this.puzzleJson.body[qIndex + 1].html),
+              this.clean(this.puzzleJson.body[qIndex + 3].html),
+              this.clean(this.puzzleJson.body[qIndex + 5].html),
+              this.clean(this.puzzleJson.body[qIndex + 7].html),
+              this.clean(this.puzzleJson.body[qIndex + 9].html),
+            ];
             this.answers = [
               this.clean(this.puzzleJson.body[aIndex].html),
               this.clean(this.puzzleJson.body[aIndex + 2].html),
               this.clean(this.puzzleJson.body[aIndex + 4].html),
               this.clean(this.puzzleJson.body[aIndex + 6].html),
               this.clean(this.puzzleJson.body[aIndex + 8].html),
+              this.clean(
+                // find kennection (last element with .html)
+                this.puzzleJson.body
+                  .slice(0)
+                  .reverse()
+                  .find((el) => {
+                    return el.html;
+                  }).html
+              ),
             ];
-            this.kennection = this.clean(
-              this.puzzleJson.body
-                .slice(0)
-                .reverse()
-                .find((el) => {
-                  return el.html;
-                }).html
-            );
+            this.image = this.puzzleJson.body
+              .slice(0)
+              .reverse()
+              .find((el) => {
+                return el.image;
+              })?.image;
 
             // clean up Kennections #1
             if (this.puzzle == 1) {
-              this.kennection = this.kennection.replace("ANSWER:", "");
               aIndex = aIndex - 1;
               this.answers = [
                 this.cleanK1(this.puzzleJson.body[aIndex].html),
@@ -155,24 +252,38 @@ export default {
                 this.cleanK1(this.puzzleJson.body[aIndex + 2].html),
                 this.cleanK1(this.puzzleJson.body[aIndex + 3].html),
                 this.cleanK1(this.puzzleJson.body[aIndex + 4].html),
+                this.answers[5].replace("ANSWER:", ""),
               ];
             }
 
+            this.questions.forEach((q, i) => {
+              this.questions[i] = q.replace(
+                /^([1-5])\.[\s]*/,
+                "<span class='number'>$1)</span> "
+              );
+            });
+
             // debug
-            this.input1 = this.answers[0];
-            this.input2 = this.answers[1];
-            this.input3 = this.answers[2];
-            this.input4 = this.answers[3];
-            this.input5 = this.answers[4];
+            this.inputs = this.answers.slice(0);
             this.inputKennection = this.kennection;
           }
         });
     },
     clean(text) {
+      // strip HTML from q & a's
       return text?.replace(/(<([^>]+)>)/gi, "").trim();
     },
     cleanK1(text) {
+      // clean up Kennections #1
       return text.split("<br/><br/>")[1].split("</p>")[0];
+    },
+    fuzzy(text) {
+      return text
+        .toLowerCase()
+        .trim()
+        .replace(/^a /, "")
+        .replace(/^the /, "")
+        .replace(/s$/, "");
     },
   },
 };
